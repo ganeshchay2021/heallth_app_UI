ffd6debcbbb30f5a6e965662edba46c82ff6b0a2
Ganesh Chaudhary <ganeshchay2021@gmail.com>
2025-01-29 20:00:29 +0545

    added schedule model

--- lib/Model/schedule_model.dart
+++ lib/Model/schedule_model.dart
@@ -15,7 +15,7 @@ class ScheduleModel {
 
 final List<ScheduleModel> scheduleDoctors = [
   ScheduleModel(
-    name: "dr. Ruben Dorwart",
+    name: "Dr. Ruben Dorwart",
     position: "Dental Specialist",
     time: '2:30 PM - 3:30 PM',
     date: 'Today',
@@ -23,7 +23,7 @@ final List<ScheduleModel> scheduleDoctors = [
         "https://images.unsplash.com/photo-1612276529731-4b21494e6d71?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZG9jdG9yJTIwcG9ydHJhaXR8ZW58MHx8MHx8fDA%3D",
   ),
     ScheduleModel(
-    name: "dr. Skylar Korsgaard",
+    name: "Dr. Skylar Korsgaard",
     position: "General Practitioner",
     time: '1:30 PM - 3:30 PM',
     date: 'Today',
@@ -31,7 +31,7 @@ final List<ScheduleModel> scheduleDoctors = [
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6F_dcVRDHxwT9RWoHyHA_Wnt9EPNJStZ0Ww&s",
   ),
     ScheduleModel(
-    name: "dr. Adison Schleifers",
+    name: "Dr. Adison Schleifers",
     position: "Dental Specialist",
     time: '6:30 PM - 8:30 PM',
     date: 'Today',
@@ -39,7 +39,7 @@ final List<ScheduleModel> scheduleDoctors = [
         "https://static.vecteezy.com/system/resources/thumbnails/026/375/249/small_2x/ai-generative-portrait-of-confident-male-doctor-in-white-coat-and-stethoscope-standing-with-arms-crossed-and-looking-at-camera-photo.jpg",
   ),
   ScheduleModel(
-    name: "dr. Adison Schleifer,",
+    name: "Dr. Adison Schleifer,",
     position: "General Practitioner",
 
     time: '10:30 - 12:30 AM',
@@ -48,7 +48,7 @@ final List<ScheduleModel> scheduleDoctors = [
         "https://media.istockphoto.com/id/1307543618/photo/team-of-doctors-and-nurses-in-hospital.jpg?s=612x612&w=0&k=20&c=-t6j5lmy_DFWtXb5HdDe0Kj6dXZwsviihuOA2lvXX5Q=",
   ),
   ScheduleModel(
-    name: "dr. Louise Reid",
+    name: "Dr. Louise Reid",
     position: "General Practitioner",
 
     time: '6:30 - 8:30 AM',
